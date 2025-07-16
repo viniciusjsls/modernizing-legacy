b0549c7d9db184d78ef9143da62035fe5b98d720
Vinicius Sacramento <viniciusjose57@gmail.com>
2025-07-16 16:34:43 +0100

    legacy - pseudo legacy controller

---
+++ LegacyModernization.Presentation/LegacyModernization.Presentation/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using LegacyModernization.Presentation.Data;
+using LegacyModernization.Presentation.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LegacyModernization.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController(AppDbContext dbContext) : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var order = dbContext.Orders.Include(x => x.Items).First(x => x.Id == id);
+
+            decimal totalPrice = order.Items.Sum(x => x.Price);
+            int numberOfItems = order.Items.Count;
+
+            return Ok(new { TotalAmount = totalPrice, NumberOfItems = numberOfItems, order.Items });
+        }
+
+        [HttpPost()]
+        public IActionResult Post([FromBody] List<OrderItem> items)
+        {
+            var order = new Order { Items = items };
+            order.Id = Guid.NewGuid();
+
+            Validate(order);
+
+            dbContext.Orders.Add(order);
+            dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        private void Validate(Order order)
+        {
+            // run some validation logic
+            if (order == null)
+            {
+                throw new Exception("Order cannot be null");
+            }
+        }
+    }
+}
