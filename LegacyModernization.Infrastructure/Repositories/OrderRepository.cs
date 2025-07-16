f997c9b0100f1902582b8b79c3abd9c0de1f236e
Vinicius Sacramento <viniciusjose57@gmail.com>
2025-07-16 20:36:38 +0100

    modernization - comments and missing include in repository

--- LegacyModernization.Infrastructure/Repositories/OrderRepository.cs
+++ LegacyModernization.Infrastructure/Repositories/OrderRepository.cs
@@ -7,9 +7,11 @@ namespace LegacyModernization.Infrastructure.Repositories
 {
     public class OrderRepository(AppDbContext dbContext) : BaseRepository(dbContext), IOrderRepository
     {
+        // we could also have a readonly repository to enforce AsNoTracking() and gain performance
+        // it would also reduce code smells in repository layer
         public async Task<Order?> Get(Guid id)
         {
-            return await dbContext.Orders.FirstOrDefaultAsync(x => x.Id == id);
+            return await dbContext.Orders.Include(x => x.Items).FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
